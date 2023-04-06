cbe8cc201cd44c12e25bf73d3530a363fd56b646
AlekseyOsipenkov <129942686+AlekseyOsipenkov@users.noreply.github.com>
2023-04-06 15:02:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math 
 
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
@@ -8,4 +9,4 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return math.log(np.prod(x))/len(x) # Ваш ответ
